48bfb06c5db72c08bcf64e228d43b0057429683a
mk <pg.studykm@gmail.com>
2021-08-02 01:04:03 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -47,10 +47,13 @@ class TasksController extends Controller
     {
         
         $request->validate([
+            
+            'status' => 'required|max:10',
             'content' => 'required|max:255',
         ]);
         
         $task = new Task;
+        $task->status = $request->status;  
         $task->content = $request->content;
         $task->save();
         
@@ -99,11 +102,13 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         $request->validate([
+            'status' => 'required|max:10',
             'content' => 'required|max:255',
         ]);
         
         $task = Task::findOrFail($id);
-        
+       
+        $message->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
