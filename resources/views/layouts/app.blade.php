ead74459b703d6f543a111f5026ef5b6dba88e3c
mk <pg.studykm@gmail.com>
2021-08-02 00:45:06 +0000

    validation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,9 @@
         @include('commons.navbar')
 
         <div class="container">
+            
+            @include('commons.error_messages')
+            
             @yield('content')
         </div>
 
